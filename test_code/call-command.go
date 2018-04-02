1e8b58c544b95892557c387c5b50f2ab5b88c340
nakamura-kyota <nakamura-kyota@SYS-Ebisu-0007.local>
2018-04-02 12:24:26 +0900

    add command test

---
+++ test_code/call-command.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+  "log"
+  "os/exec"
+)
+
+func main() {
+  log.Print("hello log!!")
+
+
+  out, error := exec.Command("date").Output()
+  if error != nil {
+      log.Printf("error: %s", error)
+      return
+  }
+
+  log.Printf("now time: %s", out)
+  log.Print("======下記はerrorのテストです========")
+
+  out2, error2 := exec.Command("false").Output()
+  if error2 != nil {
+      log.Printf("error2: %s", error2)
+      return
+  }
+
+  log.Printf("now time: %s", out2)
+}
