492ec86a711a7bf61238c76481dfbd0300fb7556
nakamura-kyota <nakamura-kyota@dmm.com>
2019-02-19 20:39:03 +0900

    add call-command3

---
+++ test-code/call-command3.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+  "log"
+  "os/exec"
+)
+
+func exec_command(command string, option string) string {
+  out, error := exec.Command(command, option).Output()
+
+  if error != nil {
+    log.Printf("エラー: %s", error)
+    return "error"
+  }
+
+  return string(out)
+}
+
+////////////////////////////////////////////////////
+// go run call-command3.go
+//
+func main() {
+	log.Print("これはコマンドを実行するプログラムです!\n")
+
+	out := exec_command("ls", "-la")
+	log.Printf("%s", out)
+}
